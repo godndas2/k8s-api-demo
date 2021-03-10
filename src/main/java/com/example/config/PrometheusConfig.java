310847849b61be67c48c5621ff240f4b93aebebb
halfdev <gusgodnd@naver.com>
2021-03-10 11:08:06 +0900

    Prometheus, Springboot data Scraping

--- src/main/java/com/example/config/PrometheusConfig.java
+++ src/main/java/com/example/config/PrometheusConfig.java
@@ -1,29 +1,18 @@
 package com.example.config;
 
-import io.prometheus.client.exporter.HTTPServer;
-import io.prometheus.client.hotspot.DefaultExports;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
-import java.io.IOException;
 
 @Configuration
 @Slf4j
 public class PrometheusConfig {
 
-    /**
-     * configuration Class가 Load 될 때 DefaultExports.initialize() 해준다.
-     */
-    @PostConstruct
-    public void init() {
-        DefaultExports.initialize(); // Export JVM Metrics into http endpoint
-    }
-
+    //  grafana에서 많은 scrape target이 있을 때 tag를 가지고 구분을 할 수 있게 해준다.
     @Bean
-    public HTTPServer promServer() throws IOException {
-        log.info("promethues server has started");
-        return  new HTTPServer(9090);
+    MeterRegistryCustomizer<MeterRegistry> meterRegistryCustomizerTags() {
+        return registry -> registry.config().commonTags("application", "PROMETHEUS-DEMO-SERVER");
     }
 }
